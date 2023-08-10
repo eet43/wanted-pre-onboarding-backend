399754fd30e4c59274806277332ee162417b365c
daehee <1998opening@gmail.com>
2023-08-10 22:25:56 +0900

    feat : Spring Security 6.1 환경설정

---
+++ src/main/java/com/example/wanted/user/security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.wanted.user.security;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private static final String[] WHITE_LIST = {
+            "/user/**",
+    };
+
+    public final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(c -> {
+                    CorsConfigurationSource source = request -> {
+                        // Cors 허용 패턴
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(
+                                List.of("*")
+                        );
+                        config.setAllowedMethods(
+                                List.of("*")
+                        );
+                        return config;
+                    };
+                    c.configurationSource(source);
+                })
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers(WHITE_LIST).permitAll())
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//                .addFilterBefore((addFilterBefore) -> {
+//                    addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+//                })
+//                .exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint((request, response, authException) -> {
+//                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "인증되지 않은 사용자입니다.");
+//                }));
+
+        return http.build();
+    }
+
+}
