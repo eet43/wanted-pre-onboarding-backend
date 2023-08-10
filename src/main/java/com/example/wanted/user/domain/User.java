329b757680f4d80b30659bb4a7878d691ad0387a
daehee <1998opening@gmail.com>
2023-08-10 23:14:45 +0900

    feat : User 도메인 생 - validation 로직 추가

---
+++ src/main/java/com/example/wanted/user/domain/User.java
@@ -0,0 +1,15 @@
+package com.example.wanted.user.domain;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+
+    public void validate() {
+        if (!this.email.contains("@")) {
+            throw new IllegalArgumentException("제대로된 이메일 형식이 아닙니다.");
+        } else if (this.password.replace(" ", "").length() < 8) {
+            throw new IllegalArgumentException("비밀번호를 8자 이상으로 입력해주세요(공백 제거)");
+        }
+    };
+}
