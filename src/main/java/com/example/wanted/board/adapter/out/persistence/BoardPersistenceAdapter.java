060ea688caae59bde95681ebac442ae070505372
daehee <1998opening@gmail.com>
2023-08-15 19:48:49 +0900

    feat : persistence Adapter 게시글 목록 조회 메서드 추가

--- src/main/java/com/example/wanted/board/adapter/out/persistence/BoardPersistenceAdapter.java
+++ src/main/java/com/example/wanted/board/adapter/out/persistence/BoardPersistenceAdapter.java
@@ -1,15 +1,21 @@
 package com.example.wanted.board.adapter.out.persistence;
 
 import com.example.wanted.board.application.port.out.ChangeBoardPort;
+import com.example.wanted.board.application.port.out.LoadBoardPort;
 import com.example.wanted.board.domain.Board;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Repository
 @RequiredArgsConstructor
-public class BoardPersistenceAdapter implements ChangeBoardPort {
+public class BoardPersistenceAdapter implements LoadBoardPort, ChangeBoardPort {
 
     private final BoardRepository boardRepository;
     private final BoardMapper boardMapper;
@@ -18,5 +24,13 @@ public class BoardPersistenceAdapter implements ChangeBoardPort {
         BoardEntity entity = boardMapper.toEntity(board);
 
         boardRepository.save(entity);
+    }
+
+    @Override
+    public List<Board> findAll(Pageable pageable) {
+        List<BoardEntity> findEntities = boardRepository.findAll(pageable).getContent();
+        return findEntities.stream()
+                .map(boardMapper::toDomain)
+                .toList();
     }
 }
